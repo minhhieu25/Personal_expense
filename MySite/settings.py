17ea0d541927911b02fc71845478b2eac72a649e
minhhieu25 <vominhhieu220825@gmail.com>
2026-06-09 15:33:17 +0700

    fix file setting.py

--- MySite/settings.py
+++ MySite/settings.py
@@ -181,7 +181,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
